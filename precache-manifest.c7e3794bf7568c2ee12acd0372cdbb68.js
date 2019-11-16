5929c5602e030f0e2e2f9699540d1e3c9bc49f8b
Kauri <kauri_che@hotmail.com>
2019-11-16 13:46:37 +1000

    Updates

---
+++ precache-manifest.c7e3794bf7568c2ee12acd0372cdbb68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "240f51d63c2a99fce3019249bc439e0f",
+    "url": "/TravelApp/index.html"
+  },
+  {
+    "revision": "f298fad714005820c190",
+    "url": "/TravelApp/static/css/main.6ba07707.chunk.css"
+  },
+  {
+    "revision": "f47daa745f6aab093156",
+    "url": "/TravelApp/static/js/2.4325ed2e.chunk.js"
+  },
+  {
+    "revision": "f298fad714005820c190",
+    "url": "/TravelApp/static/js/main.a16ecb57.chunk.js"
+  },
+  {
+    "revision": "aad832f59f03ef06ba3a",
+    "url": "/TravelApp/static/js/runtime-main.9efaa20a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/TravelApp/static/media/logo.25bf045c.svg"
+  }
+]);
